53f4d1bf0b28629f91420ef430ceec554dd4c6ec
Mehedi <developermehedi128@gmail.com>
2023-05-01 15:26:37 +0600

    vercel and link setup

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 * create server -> npm init --y
 * install express ->  npm i express
 * install nodemon -> npm i nodemon
+* insta cors -> npm i cors
+
+### server link here
+* (all Data )[https://turist-server.vercel.app/data]
+* (single Data) [https://turist-server.vercel.app/data/coxs-bazar]
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
-app.get('/',(req, res)=> {
-    res.json({ message: 'Hello Server is Running' })
+app.use(cors());
+const port = process.env.PORT || 5000;
+const data = require('./data/data.json');
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello Server is Running' });
+});
+app.get('/data', (req, res) => {
+    res.send(data);
+});
+app.get('/data/:nameSlug', (req, res) => {
+    const nameSlug = req.params.nameSlug;
+    const selectedData = data.find(n => n.nameSlug === nameSlug);
+    res.send(selectedData);
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(port, () => {
+    console.log(` API is running on port: ${port}`);
 });
